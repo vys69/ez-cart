ec693e70c1a68e4e77462984cb9f3448f84be655
grim <75869731+vys69@users.noreply.github.com>
2024-09-09 22:00:58 -0700

    ugh

---
+++ src/app/Home.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { useEffect } from 'react'
+import { useRouter } from 'next/navigation'
+
+export default function Home() {
+  const router = useRouter()
+
+  useEffect(() => {
+    const storedCurrency = localStorage.getItem("currency")
+    const storedState = localStorage.getItem("state")
+
+    if (!storedCurrency) {
+      router.push('/step/1')
+    } else if (!storedState) {
+      router.push('/step/2')
+    } else {
+      router.push('/cart')
+    }
+  }, [router])
+
+  return <div>Redirecting...</div>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,9 @@
-"use client"
-
-import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
+import Home from './Home'
 
 export function generateStaticParams() {
   return [{}]
 }
 
-export default function Home() {
-  const router = useRouter()
-
-  useEffect(() => {
-    const storedCurrency = localStorage.getItem("currency")
-    const storedState = localStorage.getItem("state")
-
-    if (!storedCurrency) {
-      router.push('/step/1')
-    } else if (!storedState) {
-      router.push('/step/2')
-    } else {
-      router.push('/cart')
-    }
-  }, [router])
-
-  return <div>Redirecting...</div>
+export default function Page() {
+  return <Home />
 }
