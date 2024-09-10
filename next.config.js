320b6deb5596a3974156dcb01a73c22199621af9
grim <75869731+vys69@users.noreply.github.com>
2024-09-09 21:50:00 -0700

    OHIOOOO

---
+++ next.config.js
@@ -0,0 +1,7 @@
+ /** @type {import('next').NextConfig} */
+ const nextConfig = {
+    output: "exports", // lightweight but no dynamic routes & api support
+    // output: "standalone", // bulkier but supports dynamic routes & api
+}
+
+module.exports = nextConfig
