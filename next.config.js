7933ae6d7d6cfbb51a50f83b5be59715a80fad57
grim <75869731+vys69@users.noreply.github.com>
2024-09-09 21:54:30 -0700

    ...

--- next.config.js
+++ next.config.js
@@ -1,7 +1,22 @@
- /** @type {import('next').NextConfig} */
- const nextConfig = {
-    output: "exports", // lightweight but no dynamic routes & api support
-    // output: "standalone", // bulkier but supports dynamic routes & api
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    trailingSlash: true,
+    distDir: 'dist',
+    images: {
+        unoptimized: true,
+    },
+    exportPathMap: async function (
+        defaultPathMap,
+        { dev, dir, outDir, distDir, buildId }
+    ) {
+        return {
+            '/': { page: '/' },
+            '/step/1': { page: '/step/[id]', query: { id: '1' } },
+            '/step/2': { page: '/step/[id]', query: { id: '2' } },
+            '/cart': { page: '/cart' },
+        }
+    },
 }
 
 module.exports = nextConfig
